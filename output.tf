d5594fc0cf7f7296a40f1baf7b3a4d04f75fea1f
Akash Kumar <akash.kumar@insight.com>
2022-11-22 20:50:05 +0530

    second commit

--- output.tf
+++ output.tf
@@ -4,9 +4,13 @@ output "vnet"{
         vnet_name=azurerm_virtual_network.example.name
         vnet_address_space=azurerm_virtual_network.example.address_space
         subnet1_name=azurerm_subnet.subnet1.name
+        subnet1_id=azurerm_subnet.subnet1.id
         subnet1_address_prefixes=azurerm_subnet.subnet1.address_prefixes
         subnet2_name=azurerm_subnet.subnet2.name
+        subnet2_id=azurerm_subnet.subnet2.id
         subnet2_address_prefixes=azurerm_subnet.subnet2.address_prefixes
+        storage_id=azurerm_storage_account.storage.id
+        dns_name=azurerm_private_dns_zone.example.name
     }
 }
 
---
+++ storage.tf
@@ -0,0 +1,12 @@
+resource "azurerm_storage_account" "storage" {
+  name                     = format("%s%s", "fdssdfd", "storage9553")
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  network_rules {
+      default_action = "Deny"
+      ip_rules = "192.168.1.31"
+  }
+}
